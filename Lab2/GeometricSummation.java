505e30a9c6780eb825a1e78dac55562c8c5bd618
Haris Nazir <haris.nazir1963@gmail.com>
2020-10-12 15:39:54 +0100

    Geometric Summation and Measuring Run Time

---
+++ Lab2/GeometricSummation.java
@@ -0,0 +1,23 @@
+public class GeometricSummation {
+
+	public static void main(String[] args) {
+		double a = 1;
+		double N = 3;
+		long start = System.currentTimeMillis();
+		double finalAnswer = geoSum(a, N);
+		long end = System.currentTimeMillis();
+		long runTime = end - start;
+		System.out.println("Final Answer: " + finalAnswer);
+		System.out.println("Run Time " + runTime);
+	}
+	
+	public static double geoSum(double a, double N) {
+		double i;
+		double sum = 0;
+		for(i = 0; i <= N; i++) {
+			sum = sum + Math.pow(a, i);
+		}
+		
+		return sum;
+	}
+}
